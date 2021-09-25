0cc97356b08473a3bc7a1d0dfb3eea5e6ad21846
bonbon195 <62191453+bonbon195@users.noreply.github.com>
2021-09-25 16:18:54 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,15 +2,16 @@ import discord
 from discord.ext import commands
 import youtube_dl
 from youtube_dl import YoutubeDL
-from functools import partial # <--------
-client = commands.Bot(command_prefix='!')
+
+client = commands.Bot(command_prefix='-')
 
 # Suppress noise about console usage from errors
 youtube_dl.utils.bug_reports_message = lambda: ''
 
+
 @client.event
 async def on_ready():
-    print("chilling as {0.user}".format(client))
+    print("second {0.user}".format(client))
 
 
 class MusicCog(commands.Cog):
@@ -18,8 +19,11 @@ class MusicCog(commands.Cog):
         self.client = client
         self.is_playing = {}
         self.music_queue = {}
-        self.ydl_opts = {'format': 'bestaudio/best', 'noplaylist': True, 'ignoreerrors': True}
-        self.FFMPEG_OPTS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+        self.ydl_opts = {'ignoreerrors': True,
+                         'format': 'bestaudio/best',
+                         'noplaylist': True}
+        self.FFMPEG_OPTS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+                            'options': '-vn'}
 
         self.vc = {}
 
@@ -27,23 +31,25 @@ class MusicCog(commands.Cog):
         with YoutubeDL(self.ydl_opts) as ydl:
             try:
                 info = ydl.extract_info(f"ytsearch:{item}", download=False)['entries'][0]
-            except Exception:
-                return False
             except TypeError:
                 return False
-        return {'source': info['formats'][0]['url'], 'title': info['title']}
+            except Exception:
+                return False
+            return {'source': info['formats'][0]['url'], 'title': info['title']}
 
-    def play_next(self, guildId):
-        if not guildId in self.music_queue: self.music_queue[guildId] = []
-        if len(self.music_queue[guildId]) > 0:
-            self.is_playing[guildId] = True
-            m_url = self.music_queue[guildId][0][0]['source']
+    def play_next(self, ctx):
+        if not ctx.guild.id in self.music_queue:
+            self.music_queue[ctx.guild.id] = []
+        if len(self.music_queue[ctx.guild.id]) > 0:
+            self.is_playing[ctx.guild.id] = True
+            m_url = self.music_queue[ctx.guild.id][0][0]['source']
 
-            self.music_queue[guildId].pop(0)
+            self.music_queue[ctx.guild.id].pop(0)
 
-            self.vc[guildId].play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTS), after=partial(self.play_next, guildId))
+            self.vc[ctx.guild.id].play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTS),
+                                       after=lambda e: self.play_next(ctx))
         else:
-            self.is_playing[guildId] = False
+            self.is_playing[ctx.guild.id] = False
 
     async def play_music(self, ctx):
         if not ctx.guild.id in self.music_queue: self.music_queue[ctx.guild.id] = []
@@ -61,18 +67,19 @@ class MusicCog(commands.Cog):
                     return
             print(self.music_queue[ctx.guild.id])
 
-
             title = ""
             title += self.music_queue[ctx.guild.id][0][0]['title']
             embed = discord.Embed(title="",
-                                    description=f":notes: Playing \n `{title}` \n - Now!\n [{ctx.author.mention}]",
-                                    color=discord.Color(0xff3aab))
+                                  description=f":notes: Playing \n `{title}` \n - Now!\n [{ctx.author.mention}]",
+                                  color=discord.Color(0xff3aab))
             await ctx.send(embed=embed)
             self.music_queue[ctx.guild.id].pop(0)
 
-            self.vc[ctx.guild.id].play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTS), after=partial(self.play_next, ctx.guild.id))
+            self.vc[ctx.guild.id].play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTS),
+                                       after=lambda e: self.play_next(ctx))
         else:
             self.is_playing[ctx.guild.id] = False
+
     @commands.command()
     async def play(self, ctx, *args):
         if not ctx.guild.id in self.music_queue: self.music_queue[ctx.guild.id] = []
@@ -89,11 +96,13 @@ class MusicCog(commands.Cog):
                 title = ""
                 title += self.music_queue[ctx.guild.id][0][0]['title']
                 embed = discord.Embed(title="",
-                                      description=f":pencil: Added song: \n `{title}` \n - To queue! \n [{ctx.author.mention}]",
+                                      description=f":pencil: Added song: \n `{title}` \n - To queue! \n "
+                                                  f"[{ctx.author.mention}]",
                                       color=discord.Color(0xff3aab))
                 await ctx.send(embed=embed)
                 if not ctx.guild.id in self.is_playing or self.is_playing[ctx.guild.id] == False:
                     await self.play_music(ctx)
+
     @commands.command()
     async def queue(self, ctx):
         if not ctx.guild.id in self.music_queue: self.music_queue[ctx.guild.id] = []
@@ -120,38 +129,36 @@ class MusicCog(commands.Cog):
 
     @commands.command()
     async def leave(self, ctx):
-        voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-        if voice.is_connected():
-            await voice.disconnect()
+        if self.vc[ctx.guild.id].is_connected():
+            await self.vc[ctx.guild.id].disconnect()
             self.is_playing[ctx.guild.id] = False
-            if not ctx.guild.id in self.music_queue: self.music_queue[ctx.guild.id] = []
+            if not ctx.guild.id in self.music_queue:
+                self.music_queue[ctx.guild.id] = []
             self.music_queue[ctx.guild.id].clear()
 
     @commands.command()
     async def pause(self, ctx):
-        voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-        if voice.is_playing():
-            voice.pause()
+        if self.is_playing[ctx.guild.id]:
+            self.vc[ctx.guild.id].pause()
         else:
             await ctx.send("No audio is playing")
 
     @commands.command()
     async def resume(self, ctx):
-        voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-        if voice.is_paused():
-            voice.resume()
+        if self.vc[ctx.guild.id].is_paused():
+            self.vc[ctx.guild.id].resume()
         else:
-            ctx.send("Audio is not paused")
+            await ctx.send("Audio is not paused")
 
     @commands.command()
     async def stop(self, ctx):
-        voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
         self.is_playing[ctx.guild.id] = False
-        voice.stop()
+        self.vc[ctx.guild.id].stop()
 
     @commands.command()
     async def clear(self, ctx):
-        if not ctx.guild.id in self.music_queue: self.music_queue[ctx.guild.id] = []
+        if not ctx.guild.id in self.music_queue:
+            self.music_queue[ctx.guild.id] = []
         self.music_queue[ctx.guild.id].clear()
         self.is_playing[ctx.guild.id] = False
         embed = discord.Embed(title="",
@@ -159,9 +166,9 @@ class MusicCog(commands.Cog):
                               color=discord.Color(0xff3aab))
         await ctx.send(embed=embed)
 
+
 client.add_cog(MusicCog(client))
 
-token = ""
-with open("token.txt") as file:
+with open("token2.txt") as file:
     token = file.read()
 client.run(token)
